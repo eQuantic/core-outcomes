fccde127d45720587a6826d2e55c84e027762e06
Edgar Mesquita <edgar.rj@gmail.com>
2019-02-23 14:59:40 +0000

    sample for outcomes

---
+++ samples/eQuantic.Core.Outcomes.Sample/Controllers/ExampleController.cs
@@ -0,0 +1,55 @@
+﻿using eQuantic.Core.Outcomes.Sample.Models;
+using eQuantic.Core.Outcomes.Sample.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eQuantic.Core.Outcomes.Sample.Controllers
+{
+    public class ExampleController : Controller
+    {
+        protected ExampleRepository ExampleRepository { get; private set; }
+        public ExampleController()
+        {
+            ExampleRepository = new ExampleRepository();
+        }
+        [HttpGet("{name}")]
+        public IActionResult Get(string name)
+        {
+            var resultBuilder = Outcome.FromItemResult<ExampleModel>();
+
+            try
+            {
+                var example = ExampleRepository.Get(name);
+                if (example == null)
+                {
+                    resultBuilder = resultBuilder
+                        .WithError()
+                        .WithStatus(ResultStatus.NotFound)
+                        .WithMessage("The example cannot be found.");
+
+                    return NotFound(resultBuilder.Result());
+                }
+
+                resultBuilder = resultBuilder
+                    .WithSuccess()
+                    .WithItem(example);
+
+                return Ok(resultBuilder.Result());
+            }
+            catch (Exception ex)
+            {
+                resultBuilder = resultBuilder
+                    .WithError()
+                    .WithStatus(ResultStatus.Error)
+                    .WithException(ex);
+
+                return StatusCode(StatusCodes.Status500InternalServerError, resultBuilder.Result());
+            }
+            
+        }
+    }
+}
---
+++ samples/eQuantic.Core.Outcomes.Sample/Repositories/ExampleRepository.cs
@@ -0,0 +1,28 @@
+﻿using eQuantic.Core.Outcomes.Sample.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eQuantic.Core.Outcomes.Sample.Repositories
+{
+    public class ExampleRepository
+    {
+        private List<ExampleModel> _examples = new List<ExampleModel>
+        {
+            new ExampleModel { Name = "example1", Description = "The example 1" },
+            new ExampleModel { Name = "example2", Description = "The example 2" },
+            new ExampleModel { Name = "example3", Description = "The example 3" }
+        };
+
+        public ExampleModel Get(string name)
+        {
+            return _examples.FirstOrDefault(e => e.Name == name);
+        }
+
+        public IEnumerable<ExampleModel> GetAll()
+        {
+            return _examples;
+        }
+    }
+}
